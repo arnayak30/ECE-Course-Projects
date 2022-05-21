4ffcc98a2321b66c92608e571be22e56ac2d9912
Abhinav (Abhi) Nayak <52806057+arnayak30@users.noreply.github.com>
2022-05-20 19:06:52 -0500

    Delete lab8 directory

--- lab8/sliding.c
+++
@@ -1,7 +0,0 @@
-#include "sliding.h"
-/*  Slide all values of array up
-*/
-void slide_up(int* my_array, int rows, int cols){
-
-    return;
-}
--- lab8/sliding.h
+++
@@ -1,5 +0,0 @@
-#ifndef SLIDING_H
-#define SLIDING_H
-void slide_up(int* my_array, int rows, int cols);
-
-#endif
