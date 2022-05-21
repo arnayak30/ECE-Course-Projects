badb1f51441232a2d07a8a80e9e862c572b2bc25
Abhinav (Abhi) Nayak <52806057+arnayak30@users.noreply.github.com>
2022-05-20 19:06:20 -0500

    Delete lab4 directory

--- lab4/prime.c
+++
@@ -1,7 +0,0 @@
-#include "prime.h"
-
-int is_prime(int n){
-  // Return 1 if n is a prime, and 0 if it is not
-  return 0;
-}
-
--- lab4/prime.h
+++
@@ -1,6 +0,0 @@
-#ifndef PRIME_H
-#define PRIME_H
-
-int is_prime(int n);
-
-#endif
